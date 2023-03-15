ee8faf43953bfecc4ef2c2725cd5688a774e4493
Jonathan Dobson <jdobson@redhat.com>
2023-03-15 16:40:06 -0600

    UPSTREAM: <carry>: STOR-1270: Restore carry patch that enables CSI migration in any order

---
+++ pkg/features/patch_kube_features.go
@@ -0,0 +1,23 @@
+package features
+
+import (
+	"k8s.io/apimachinery/pkg/util/runtime"
+	utilfeature "k8s.io/apiserver/pkg/util/feature"
+	"k8s.io/component-base/featuregate"
+)
+
+var (
+	// owner: @fbertina
+	// beta: v1.23
+	//
+	// Enables the vSphere CSI migration for the Attach/Detach controller (ADC) only.
+	ADCCSIMigrationVSphere featuregate.Feature = "ADC_CSIMigrationVSphere"
+)
+
+var ocpDefaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureSpec{
+	ADCCSIMigrationVSphere: {Default: true, PreRelease: featuregate.GA},
+}
+
+func init() {
+	runtime.Must(utilfeature.DefaultMutableFeatureGate.Add(ocpDefaultKubernetesFeatureGates))
+}
